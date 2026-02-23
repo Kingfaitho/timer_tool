eed53c80751879d0a17a085c5f2095072e946504
Kingfaitho <faithkay49@gmail.com>
2026-02-23 18:02:47 +0100

    Initial commit

---
+++ target/debug/deps/timer_tool.d
@@ -0,0 +1,5 @@
+C:\Users\user\projectz\rustHello\timer_tool\target\debug\deps\timer_tool.d: src\main.rs
+
+C:\Users\user\projectz\rustHello\timer_tool\target\debug\deps\timer_tool.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/timer_tool.d
@@ -0,0 +1,1 @@
+C:\Users\user\projectz\rustHello\timer_tool\target\debug\timer_tool.exe: C:\Users\user\projectz\rustHello\timer_tool\src\main.rs
